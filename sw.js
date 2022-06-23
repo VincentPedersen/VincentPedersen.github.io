cc766d4285cc2920053ca6f6c331c010bb7d85f1
Vincent <vipe20ad@student.cbs.dk>
2022-06-23 15:15:39 +0200

    Hopefully last changes before making it be a PWA

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./","./src/master.css","./Images/logo192.png"])
+        })
+    );
+});
+
+
+self.addEventListener("fetch", e => {
+    console.log(`intercepting fetch request for: ${e.request.url}`);
+});
